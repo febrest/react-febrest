2decc2717648842077136dada2e2f75e9f090e89
Spencer <pallyoung@163.com>
2018-03-13 13:58:28 +0800

    test

--- examples/todomvc/index.js
+++ examples/todomvc/index.js
@@ -11,33 +11,86 @@ function Header() {
         </h1>
     );
 }
+function Input(props) {
+    return (
+        <p className="new-todo-container">
+            <input
+                className="new-todo"
+                onKeyPress={props.onKeyPress}
+                placeholder="需要做什么？" />
+        </p>
+    );
+}
+
+class List extends React.Component {
+    render() {
+        return (
+            <ul className="list">
+            </ul>
+        );
+    }
+}
+
+function Footer(props) {
+    var onItemClick = props.onItemClick || function () { };
+    var selected = props.selected;
+    return (
+        <footer className="selector">
+            <p className="filters">
+                <span
+                    onClick={() => onItemClick(0)}
+                    className={selected===0?"all selected":''}>all
+                    </span>
+                <span
+                    className={selected===1?"all selected":''}
+                    onClick={() => onItemClick(1)}>
+                    active
+                    </span>
+                <span
+                    className={selected===2?"all selected":''}
+                    onClick={() => onItemClick(2)}>
+                    complete
+                    </span>
+            </p>
+        </footer>
+    );
+}
 class App extends React.Component {
+    constructor(...props) {
+        super(...props);
+        this.state = {
+            selected:0
+        }
+    }
+    componentDidMount() {
+        
+    }
+    
+    componentWillUnmount() {
+        
+    }
+    _onItemClick(selected){
+        this.setState({selected})
+    }
+    _enter(e){
+        if (e.target.value == '') {
+            return;
+        }
+        if (e.target.className === 'new-todo' && e.key === 'Enter') {
+            e.target.value = '';
+        }
+    }
     render() {
         return (
             <div className="container">
                 <Header />
                 <section className="main">
-                    <p className="new-todo-container">
-                        <input className="new-todo" placeholder="需要做什么？" />
-                    </p>
-                    <ul className="list">
-                    </ul>
-                    <footer className="selector">
-                        <p className="filters">
-                            <span
-                                fbclick="app.changeType('all')"
-                                className="all selected">all
-                    </span>
-                            <span
-                                fbclick="app.changeType('active')">
-                                active
-                    </span>
-                            <span
-                                fbclick="app.changeType('complete')">
-                                complete
-                    </span>
-                        </p>
-                    </footer>
+                    <Input 
+                        onKeyPress = {(event)=>this._enter(event)}/>
+                    <List />
+                    <Footer
+                        selected={this.state.selected}
+                        onItemClick={(index)=>this._onItemClick(index)} />
                 </section>
             </div>
         );
