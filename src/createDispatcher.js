7e64eaf7e05b7494b09a166830cb034d0fecf273
Spencer <pallyoung@163.com>
2018-03-26 17:23:37 +0800

    listener is this

--- src/createDispatcher.js
+++ src/createDispatcher.js
@@ -8,22 +8,26 @@ var {
 
 function createDispatcher(componentInst, onData) {
     var idMap = {};
-    function _onData(data) {
+    function _onData(data, isThis) {
         if (onData) {
-            return onData.call(componentInst, data);
+            return onData.call(componentInst, data, isThis);
         } else {
             return false;
         }
     }
-    function dispatch(key,payload) {
-        var id = Febrest.dispatch(key,payload);
+    function dispatch(key, payload) {
+        var id = Febrest.dispatch(key, payload);
         idMap[id] = true;
+        return id;
     }
     function listener(data) {
-        var result = _onData(data);
-        if(!result && idMap[data.id]){
+        var id = data.id;
+        var isThis = !!idMap[id];
+        var result = _onData(data, isThis);
+        if (!result && isThis) {
             componentInst.setState(data.state);
         }
+        delete idMap[id];
     }
     subscribe(listener);
     function release() {
